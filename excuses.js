277ea6bc6d44d595eebdbd5d895e4bd61bc60993
mrmichaelchapo-del <mr.michaelchapo@gmail.com>
2026-09-11 18:54:25 -0400

    Update excuses.js

--- excuses.js
+++ excuses.js
@@ -9,3 +9,26 @@ console.log("That wasn't me, that was a deer in the house.");
 
 // Excuse 4
 console.log("That was my computer malfunctioning, not me.");
+// Excuse 5
+console.log("I wasn’t up late, I was waiting for you to say 'bedtime' a second time.");
+
+// Excuse 6
+console.log("I didn’t touch the remote, I was just checking if you were still watching.");
+
+// Excuse 7
+console.log("I wasn’t running, I was hurrying because you said 'come here' with urgency.");
+
+// Excuse 8
+console.log("I didn’t spill anything, I was testing if the cup was kid-proof like you said.");
+
+// Excuse 9
+console.log("I wasn’t yelling, I was projecting my voice so you could hear me over your phone call.");
+
+// Excuse 10
+console.log("I didn’t leave the lights on, I left them for safety because you always say 'don’t trip'.");
+
+// Excuse 11
+console.log("I wasn’t sneaking snacks, I was checking if I needed to eat before dinner like you always ask me.");
+
+// Excuse 12
+console.log("I didn’t make the noise, I was investigating the noise so you wouldn’t have to get up.");
