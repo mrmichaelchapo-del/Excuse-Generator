ccd5d6ddf90a1ab6555ed23effb2a0ecfea73ab0
mrmichaelchapo-del <mr.michaelchapo@gmail.com>
2026-09-11 18:09:07 -0400

    Update excuses.js

--- excuses.js
+++ excuses.js
@@ -6,3 +6,6 @@ console.log("Mom, that was the monster downstairs. That wasn't me.");
 
 // Excuse 3
 console.log("That wasn't me, that was a deer in the house.");
+
+// Excuse 4
+console.log("That was my computer malfunctioning, not me.");
