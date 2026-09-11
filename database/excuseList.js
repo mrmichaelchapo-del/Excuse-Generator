905d59b4f11bb0e94a84338c6f6b0283ad127d1f
mrmichaelchapo-del <mr.michaelchapo@gmail.com>
2026-09-11 18:51:08 -0400

    Create excuseList.js

---
+++ database/excuseList.js
@@ -0,0 +1,6 @@
+const excuses = [
+  "I didn't sneak the phone, my brother snuck it in my room.",
+  "Mom, that was the monster downstairs. That wasn't me.",
+  "That wasn't me, that was a deer in the house.",
+  "That was my computer malfunctioning, not me."
+];
