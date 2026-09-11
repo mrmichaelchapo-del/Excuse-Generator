4b3a48f79dcf134c715f29644d2921947a65048a
mrmichaelchapo-del <mr.michaelchapo@gmail.com>
2026-09-11 18:03:44 -0400

    Update excuses.js

--- excuses.js
+++ excuses.js
@@ -2,4 +2,7 @@
 console.log("I didn't sneak the phone, My brother snuck it in my room.");
 
 // Excuse 2
-console.log("Mom, that was the monster downstairs. That wasn't me.")
+console.log("Mom, that was the monster downstairs. That wasn't me.");
+
+// Excuse 3
+console.log("That wasn't me, that was a deer in the house.");
